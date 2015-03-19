8fd248af05d8299b2a829090d8f441df0f795718
Michael Moncada <michael.moncada@utoronto.ca>
2015-03-19 17:06:56 -0400

    Adding shader boiler plate code and segmenting off shaders into seperate views.

--- www/scripts/angular-app/services/webGLUtilityModule.js
+++ www/scripts/angular-app/services/webGLUtilityModule.js
@@ -12,7 +12,118 @@ angular.module('webGLUtilityModule', [])
           __canvasType = ((type === _Canvas.prototype.CANVAS_TYPES.CANVAS_3D) ? _Canvas.prototype.CANVAS_TYPES.CANVAS_3D : _Canvas.prototype.CANVAS_TYPES.CANVAS_2D),
           __height = 480,
           __width = 640,
-          __viewportNeedsUpdate = true;
+          __viewportNeedsUpdate = true,
+          __glProgram = null,
+          __shaders = {};
+
+
+      function __initProgram() {
+          if (__context === null || __canvasType !== _Canvas.prototype.CANVAS_TYPES.CANVAS_3D) {
+            return;
+          }
+
+          // create a program.
+          __glProgram = __context.createProgram();
+
+           // attach the shaders.
+           var  vertexShaderResource = __shaders[_Canvas.prototype.SHADER_TYPES.VERTEX].resource,
+                fragmentShaderResource = __shaders[_Canvas.prototype.SHADER_TYPES.FRAGMENT].resource;
+
+           if (vertexShaderResource) {
+            __context.attachShader(__glProgram, vertexShader);
+           }
+
+           if (fragmentShaderResource) {
+            __context.attachShader(__glProgram, fragmentShaderResource);
+           }
+
+
+           __context.attachShader(__glProgram, fragmentShader);
+
+           // link the program.
+           __context.linkProgram(__glProgram);
+
+           // Check if it linked.
+           var success = __context.getProgramParameter(__glProgram, __context.LINK_STATUS);
+
+           if (! success) {
+               // something went wrong with the link
+               throw new Error('Program filed to link: ' + __context.getProgramInfoLog(__glProgram));
+           }
+
+           __context.useProgram(__glProgram);
+      }
+
+
+      function __getShaderContentsHelper(shaderStr) {
+        var shaderContents = null;
+
+
+        if (shaderStr[0] === '#') {
+          var idDOM = $(shaderStr);
+
+          if (idDOM.length) {
+            shaderContents = idDOM.text();
+          }
+        }
+
+        return shaderContents;
+      }
+
+
+      function __setShaders(vertexShader, fragmentShader) {
+
+        if (! __context || typeof vertexShader !== 'string' || typeof fragmentShader !== 'string' || __canvasType !== ___Canvas.prototype.CANVAS_TYPES.CANVAS_3D) {
+          throw new Error('Could not set the vertex/fragment shaders!\nYou sent vertex shader contents: ' +
+                            vertexShader + '\nYou sent fragment shader contents: ' + fragmentShader);
+        }
+
+        var vsContents = __getShaderContentsHelper(vertexShader),
+            fsContents = __getShaderContentsHelper(fragmentShader);
+
+
+        compiledVertexShader = __compileShader(vsContents, __context.VERTEX_SHADER);
+        compiledFragmentShader = __compileShader(fsContents, __context.FRAGMENT_SHADER);
+
+
+        var vs = shaders[_Canvas.prototype.SHADER_TYPES.VERTEX],
+            fs = shaders[_Canvas.prototype.SHADER_TYPES.FRAGMENT];
+
+        vs.src = vsContents, vs.resource = compiledVertexShader;
+        fs.src = fsContents, fs.resource = compiledFragmentShader;
+
+        __initProgram();
+
+      }
+
+
+      function __compileShader(shaderSource, shaderType) {
+
+        if (__context === null || __canvasType !== _Canvas.prototype.CANVAS_TYPES.CANVAS_3D || typeof shaderSource !== 'string') {
+          return null;
+        }
+
+        var realShaderType = ((shaderType === _Canvas.prototype.SHADER_TYPES.VERTEX) ? __context.VERTEX_SHADER : __context.FRAGMENT_SHADER);
+
+        // Create the shader object
+        var shader = __context.createShader(realShaderType);
+
+        // Set the shader source code.
+        __context.shaderSource(shader, shaderSource);
+
+        // Compile the shader
+        __context.compileShader(shader);
+
+        // Check if it compiled
+        var success = __context.getShaderParameter(shader, __context.COMPILE_STATUS);
+
+        if (! success ) {
+          // Something went wrong during compilation; get the error
+          throw new Error('Could not compile ' + shaderType +  ' shader: ' + __context.getShaderInfoLog(shader));
+        }
+
+        return shader;
+      }
 
 
       function __initCanvas() {
@@ -30,7 +141,13 @@ angular.module('webGLUtilityModule', [])
         __initContext();
         __updateViewportSize();
 
+        __shaders[_Canvas.prototype.SHADER_TYPES.VERTEX] = {src: '', resource: null};
+        __shaders[_Canvas.prototype.SHADER_TYPES.FRAGMENT] = {src: '', resource: null};
 
+      }
+
+      function __getVShadersObj() {
+        return __shaders;
       }
 
 
@@ -106,6 +223,9 @@ angular.module('webGLUtilityModule', [])
         return __context;
       };
 
+
+      this.setVertexAndFragmentShaders = __setShaders;
+
       this.updateViewportSize = __updateViewportSize;
 
 
@@ -126,6 +246,10 @@ angular.module('webGLUtilityModule', [])
       CANVAS_TYPES: {
         CANVAS_2D: '2d',
         CANVAS_3D: 'webgl'
+      },
+      SHADER_TYPES: {
+        VERTEX: 'vertex',
+        FRAGMENT: 'fragment'
       },
       constructor: _Canvas
     };
