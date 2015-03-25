adf5c188e2e4ed9b13998ce0053b8bd23e499540
Michael Moncada <michael.moncada@utoronto.ca>
2015-03-25 08:02:57 -0400

    Forgot to turn on the ngTouch module.

--- www/scripts/angular-app/app.js
+++ www/scripts/angular-app/app.js
@@ -1,7 +1,7 @@
 'use strict';
 
 // Declare app level module which depends on views, and components
-angular.module('WebGLRLOApp', [/*'ngAria', 'ngTouch', */ 'webGLUtilityModule'])
+angular.module('WebGLRLOApp', [/*'ngAria',*/ 'ngTouch', 'webGLUtilityModule'])
   .config(function ($compileProvider) {
     //We don't need routing, because the ng-controller will load automagically
     $compileProvider.aHrefSanitizationWhitelist(/^\s*(https?|mailto|javascript):/);
